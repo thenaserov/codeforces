7d9cb1f379eeadbb816697f1986f7975ff8f04ba
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2025-05-18 21:56:53 +0330

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    if (w == 0){ cout << "NO" << endl;}
+    if(w%2!=0){
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+    }
+}
