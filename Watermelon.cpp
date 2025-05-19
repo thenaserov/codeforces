6c09bce38278f97f47d5809055b31a1ed680af04
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2025-05-19 23:14:08 +0330

    Update Watermelon.cpp

--- Watermelon.cpp
+++ Watermelon.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main() {
     int w;
     cin >> w;
-    if (w == 0){ cout << "NO" << endl;}
-    if(w%2!=0){
-        cout << "NO" << endl;
-    } else {
-        cout << "YES" << endl;
+    if (w == 2){
+        cout << "NO";
+    }
+    else if(w%2==0){
+        cout << "YES";
+    } 
+    else {
+        cout << "NO";
     }
 }
